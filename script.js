9a9e623e2716225e2169a07ccbc251ea595e9b33
takinabradley <bradfinney98@gmail.com>
2021-10-29 18:56:34 -0400

    Added a '+/-' button on the calculator that allows the initial input to be a negative number.

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ function getDisplayContent () {
   return document.querySelector('.display').textContent;
 }
 
+
 function clearDisplayContent () {
   document.querySelector('.display').textContent = '';
   document.querySelector('.decimal').classList.remove('used');
@@ -47,21 +48,39 @@ function clearDisplayContent () {
     after being cleared */
 }
 
+
 function allowNumInput() {
   const numPad = document.querySelectorAll('.num');
   
   numPad.forEach( num => num.addEventListener('click', (num) => {
     const display = document.querySelector('.display');
     const decimal = document.querySelector('.decimal');
+    const negative = document.querySelector('.negative');
     const numContent = num.target.textContent;
-
+    
     if (decimal.classList.contains('used') && numContent === '.') return;
+    
+    if (!negative.classList.contains('active') && numContent === '+/-') {
+      display.textContent = display.textContent.padStart(display.textContent.length + 1, '-');
+      negative.classList.add('active');
+      return;
+    } else if (negative.classList.contains('active') && numContent === '+/-') {
+      display.textContent = display.textContent.slice(1);
+      negative.classList.remove('active');
+      return;
+    }
     
     display.textContent += numContent;
     display.textContent.includes('.') ? decimal.classList.add('used') : 
-                                        decimal.classList.remove('used');
+                            decimal.classList.remove('used');
   }));
 }
 
 
+
 allowNumInput();
+
+//input1 -> operator -> input2 -> operator/equals => input1 -> output input1 ...repeat
+//can use leftover input2 to continue doing the operation on every equal operator press.
+//reassign input two only after additional outputs? Clear button should allow user to choose input1 again. (clear the array)
+//add a -/+ button?
